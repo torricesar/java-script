d5cfa8fc53e951d7cd47deaa9617342a13c2e033
Túlio C. Torri <93161398+torricesar@users.noreply.github.com>
2022-05-23 13:10:50 -0300

    if else primeira parte

---
+++ ex001/js.js
@@ -0,0 +1,12 @@
+var idade = prompt ("Qual sua idade?")
+
+
+
+
+if(idade < 16){
+    alert ("Não vota")
+} else if(idade < 18 || idade > 65) {
+        alert ("Voto opcional")
+    } else if(idade > 18) {
+        alert ("voto obrigatório")
+        } 
